0d3c73f447c84df8ce9d682d8589d75fdfe65dcc
ent3r <32072697+ent3r@users.noreply.github.com>
2020-11-06 14:45:09 +0100

    docs: remove vuepress generated content and insert custom
    Content now matches the actual project

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,10 +1,10 @@
-const { description } = require('../../package')
+const { description } = require("../../package");
 
 module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: "CTFNote",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -16,9 +16,12 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
   ],
 
   /**
@@ -27,44 +30,33 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
+    repo: "",
     editLinks: false,
-    docsDir: '',
-    editLinkText: '',
+    docsDir: "",
+    editLinkText: "",
     lastUpdated: false,
     nav: [
       {
-        text: 'Guide',
-        link: '/guide/',
+        text: "Guide",
+        link: "/",
       },
       {
-        text: 'Config',
-        link: '/config/'
+        text: "Config",
+        link: "/",
       },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
-      }
     ],
     sidebar: {
-      '/guide/': [
+      "/": [
         {
-          title: 'Guide',
+          title: "Homepage",
           collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
+        },
       ],
-    }
+    },
   },
 
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
-}
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+};
--- src/index.md
+++ src/index.md
@@ -1,15 +1,15 @@
 ---
 home: true
-heroImage: https://v1.vuepress.vuejs.org/hero.png
+heroImage: https://avatars0.githubusercontent.com/u/72804071
 tagline: The documentation page for CTFNote, both backend and frontend
 actionText: Quick Start →
-actionLink: /guide/
+actionLink: /
 features:
-- title: Feature 1 Title
-  details: Feature 1 Description
-- title: Feature 2 Title
-  details: Feature 2 Description
-- title: Feature 3 Title
-  details: Feature 3 Description
+  - title: MongoDB as a database
+    details: Easy to install and use
+  - title: AAA
+    details: Feature 2 Description
+  - title: Feature 3 Title
+    details: Feature 3 Description
 footer: Made by S1LV3R with ❤️
 ---
